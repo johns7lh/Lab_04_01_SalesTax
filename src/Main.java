a0c372c39234e99bf8fc4fb684cc3ba3ce8b39a2
johns7lh <jlauren1111@yahoo.com>
2023-09-17 15:06:15 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+
+    public static void main(String[] args)
+    {
+    double pricePurchase = 34.56;
+
+    System.out.println("The price of your item is " + pricePurchase + " and the tax is " + pricePurchase *.05);
+
+
+
+
+    }
+}
